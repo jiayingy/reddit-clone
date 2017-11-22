706004e3006fed13b83c3acc6a8108c60c95cf8d
jiayingy <jiayinglek@gmail.com>
2017-11-22 19:55:04 +0800

    Create base entryList component

---
+++ src/components/EntryList.vue
@@ -0,0 +1,17 @@
+<template>
+  <Entry/>
+</template>
+<script>
+import Entry from './Entry';
+
+export default {
+  components: {
+    Entry,
+  },
+};
+</script>
+<style lang="scss">
+
+</style>
+
+
