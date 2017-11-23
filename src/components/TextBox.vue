99ab45f992846f6cc74a3e514407e4e607e40fcc
jiayingy <jiayinglek@gmail.com>
2017-11-23 16:58:36 +0800

    Style textbox for adding titles

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -6,11 +6,37 @@
 </template>
 <script>
 export default {
-  
+
 };
 </script>
 <style lang="scss">
+.add-title {
+  height: 60px;
+  padding: 20px;
+  background-color: white;
+  box-shadow: 0 1px 5px 1px rgba(0,0,0,0.2);
+  position: relative;
 
+  input {
+    padding: 15px;
+    width: 50%;
+    font-size: 16px;
+    background-color: #eee;
+    border: 0;
+    outline: none;
+  }
+
+  button {
+    // position: absolute;
+    margin-left: -5px;
+    padding: 15px;
+    border: 0;
+    font-size: 16px;
+    background-color: #d3edc9;
+    border-radius: 0;
+    outline: none;
+  }
+}
 </style>
 
 
