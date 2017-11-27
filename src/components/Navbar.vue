29072bc0e6ac3523e01b9f17762d3f8309640565
jiayingy <jiayinglek@gmail.com>
2017-11-27 18:06:11 +0800

    Change active tabs when clicked

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="navbar">
     <ul>
-      <li class="active">Popular</li>
-      <li>New</li>
-      <li>All</li>
+      <li class="active"
+          @click="switchTab('popular', $event)">
+          Popular
+      </li>
+      <li
+          @click="switchTab('new', $event)">
+          New
+      </li>
+      <li
+          @click="switchTab('all', $event)">
+          All
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Navbar',
+  data() {
+    return {
+      currentTab: 'popular',
+    };
+  },
+  methods: {
+    switchTab(tab, event) {
+      if (tab !== this.currentTab) {
+        // Remove 'active' class from all other elments and add to current element
+        document.querySelector('.active').classList.remove('active');
+        event.target.classList.add('active');
 
+        // Update value of currentTab
+        this.currentTab = tab;
+
+        // this.$emit('switchTab', tab);
+      }
+    },
+  },
 };
 </script>
 
