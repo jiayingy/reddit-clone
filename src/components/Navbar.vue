0aee3b0372c87de4cf5b7878aa0e2d0a9c00e576
jiayingy <jiayinglek@gmail.com>
2017-11-27 17:34:21 +0800

    Create base navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="navbar">
+    <ul>
+      <li class="active">Popular</li>
+      <li>New</li>
+      <li>All</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+.navbar {
+  ul {
+    list-style: none;
+    padding-left: 0;
+    li {
+      display: inline-block;
+      margin: 10px 50px;
+      cursor: pointer;
+
+      &.active {
+        font-weight: bold;
+        border-bottom: 1px dotted black;
+      }
+    }
+  }
+}
+</style>
+
+
