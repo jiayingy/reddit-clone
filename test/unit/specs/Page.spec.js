051a9a06e63823252c0a481531ff9a18dc8ee267
jiayingy <jiayinglek@gmail.com>
2017-11-28 17:39:58 +0800

    Test Page's addNewTitle function by checking if number of entries increases after addition

--- test/unit/specs/Page.spec.js
+++ test/unit/specs/Page.spec.js
@@ -8,7 +8,22 @@ const page = new PageConstructor().$mount();
 describe('Page.vue', () => {
   context('when adding a valid title', () => {
     it('should increase length of entries by 1', () => {
-      const initialNumEntries = page.entries.length;
+      // Get initial number of entries
+      const initialNumEntries = Object.keys(page.entries).length;
+
+      // Create entry object to be added in
+      const entryObj = {
+        title: 'this is a test title',
+        numVotes: 0,
+      };
+
+      // Add new title to entries
+      page.addNewTitle(entryObj);
+
+      // Get number of entries after adding
+      const numEntries = Object.keys(page.entries).length;
+
+      expect(numEntries).to.equal(initialNumEntries + 1);
     });
   });
 });
