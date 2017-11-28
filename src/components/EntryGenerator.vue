eaf479ac414f33cb7953e7c97761b096d2af6290
jiayingy <jiayinglek@gmail.com>
2017-11-28 17:09:39 +0800

    Extract num votes as a variable and populate 30 new titles on mount

--- src/components/EntryGenerator.vue
+++ src/components/EntryGenerator.vue
@@ -17,14 +17,24 @@ export default {
   props: ['value'],
   methods: {
     addNewTitle() {
-      this.$emit('addNewTitle', { title: this.newTitle, numVotes: 0 });
+      this.$emit('addNewTitle', { title: this.newTitle, numVotes: this.numVotes });
       this.newTitle = '';
+      this.numVotes = 0;
     },
   },
   data() {
     return {
       newTitle: '',
+      numVotes: 0,
     };
+  },
+  mounted() {
+    // Populate 30 new titles on mount with random num votes
+    for (let i = 0; i < 30; i += 1) {
+      this.newTitle = `This is a title${i}`;
+      this.numVotes = Math.floor(Math.random() * ((1000 - (-234)) + 1)) - 234;
+      this.addNewTitle();
+    }
   },
 };
 </script>
