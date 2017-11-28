281f9058747afaa177357648f26e33c750f2ea6b
jiayingy <jiayinglek@gmail.com>
2017-11-28 18:38:19 +0800

    Add test for EntryGenerator when title is invalid

--- test/unit/specs/EntryGenerator.spec.js
+++ test/unit/specs/EntryGenerator.spec.js
@@ -6,6 +6,8 @@ const EntryGeneratorConstructor = Vue.extend(EntryGenerator);
 const eg = new EntryGeneratorConstructor().$mount();
 
 describe('EntryGenerator.vue', () => {
+  let emittedObjTitle;
+  let emittedObjVotes;
   context('when adding a valid title with button click', () => {
     it('should emit an object containing the same title and numvotes', () => {
       const title = 'a test title';
@@ -13,9 +15,6 @@ describe('EntryGenerator.vue', () => {
 
       eg.newTitle = title;
       eg.numVotes = votes;
-
-      let emittedObjTitle;
-      let emittedObjVotes;
 
       eg.$on('addNewTitle', (obj) => {
         emittedObjTitle = obj.title;
@@ -25,6 +24,21 @@ describe('EntryGenerator.vue', () => {
       eg.$el.querySelector('button').click();
       expect(emittedObjTitle).to.equal(title);
       expect(emittedObjVotes).to.equal(votes);
+    });
+  });
+  context('when adding an invalid title with button click', () => {
+    it('should not emit addNewTitle function', () => {
+      let eventEmitted = false;
+
+      eg.$on('addNewTitle', (obj) => {
+        emittedObjTitle = obj.title;
+        emittedObjVotes = obj.numVotes;
+        eventEmitted = true;
+      });
+
+      eg.$el.querySelector('button').click();
+
+      expect(eventEmitted).to.equal(false);
     });
   });
 });
