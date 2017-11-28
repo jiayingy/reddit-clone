96c953935ddb55ea2546802ef651e09c8d6a016e
jiayingy <jiayinglek@gmail.com>
2017-11-28 18:23:00 +0800

    Create unit test for entrylist computed results to determine if input and output are the same

--- test/unit/specs/EntryList.spec.js
+++ test/unit/specs/EntryList.spec.js
@@ -6,9 +6,24 @@ const EntryListConstructor = Vue.extend(EntryList);
 const entrylist = new EntryListConstructor().$mount();
 
 describe('EntryList.vue', () => {
-  context('when given a list of entries with different number of votes', () => {
-    it('should sort entries in descreasing order according to number of votes', () => {
-      expect().to.equal();
+  context('when given a hashmap of entries with different number of votes', () => {
+    it('should return an array of entries containing the same title and votes', () => {
+      entrylist.value = {};
+
+      for (let i = 0; i < 5; i += 1) {
+        entrylist.$set(entrylist.value, i, {
+          id: i,
+          title: `this is a title ${i}`,
+          numVotes: i,
+        });
+      }
+
+      const arr = entrylist.results;
+
+      for (let i = 0; i < 5; i += 1) {
+        expect(arr[i].title).to.equal(`this is a title ${i}`);
+        expect(arr[i].numVotes).to.equal(i);
+      }
     });
   });
 });
