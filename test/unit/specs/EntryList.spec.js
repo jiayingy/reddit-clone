6ce7d33dc4e4423f837257816fa3cee8214ba18f
jiayingy <jiayinglek@gmail.com>
2017-11-28 18:29:15 +0800

    Test entrylist computed popularTitles to return an array sorted by votes in descending order

--- test/unit/specs/EntryList.spec.js
+++ test/unit/specs/EntryList.spec.js
@@ -5,24 +5,36 @@ import EntryList from '@/components/EntryList';
 const EntryListConstructor = Vue.extend(EntryList);
 const entrylist = new EntryListConstructor().$mount();
 
+const maxEntries = 5;
+
 describe('EntryList.vue', () => {
+  // Create a hashmap with 5 entries
+  entrylist.value = {};
+
+  for (let i = 0; i < maxEntries; i += 1) {
+    entrylist.$set(entrylist.value, i, {
+      id: i,
+      title: `this is a title ${i}`,
+      numVotes: i,
+    });
+  }
+
   context('when given a hashmap of entries with different number of votes', () => {
     it('should return an array of entries containing the same title and votes', () => {
-      entrylist.value = {};
-
-      for (let i = 0; i < 5; i += 1) {
-        entrylist.$set(entrylist.value, i, {
-          id: i,
-          title: `this is a title ${i}`,
-          numVotes: i,
-        });
-      }
-
       const arr = entrylist.results;
 
-      for (let i = 0; i < 5; i += 1) {
+      for (let i = 0; i < maxEntries; i += 1) {
         expect(arr[i].title).to.equal(`this is a title ${i}`);
         expect(arr[i].numVotes).to.equal(i);
+      }
+    });
+
+    it('should return an array of entries sorted by number of votes in descending order', () => {
+      const arr = entrylist.popularTitles;
+
+      for (let i = 0; i < maxEntries; i += 1) {
+        expect(arr[i].title).to.equal(`this is a title ${maxEntries - i - 1}`);
+        expect(arr[i].numVotes).to.equal(maxEntries - i - 1);
       }
     });
   });
