6ee0ffc8b968a8a68d700ff0c16614ffa7731d7f
jiayingy <jiayinglek@gmail.com>
2017-11-28 17:29:46 +0800

    Create unit test for EntryGenerator's addNewTitle function

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,30 @@
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld';
+import EntryGenerator from '@/components/EntryGenerator';
 
-describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
-    .to.equal('Welcome to Your Vue.js App');
+// Set up EntryGenerator component
+const EntryGeneratorConstructor = Vue.extend(EntryGenerator);
+const eg = new EntryGeneratorConstructor().$mount();
+
+describe('EntryGenerator.vue', () => {
+  context('when adding a valid title with button click', () => {
+    it('should emit an object containing the same title and numvotes', () => {
+      const title = 'a test title';
+      const votes = 5;
+
+      eg.newTitle = title;
+      eg.numVotes = votes;
+
+      let emittedObjTitle;
+      let emittedObjVotes;
+
+      eg.$on('addNewTitle', (obj) => {
+        emittedObjTitle = obj.title;
+        emittedObjVotes = obj.numVotes;
+      });
+
+      eg.$el.querySelector('button').click();
+      expect(emittedObjTitle).to.equal(title);
+      expect(emittedObjVotes).to.equal(votes);
+    });
   });
 });
